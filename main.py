8794eb1f89d6602eb94c616e67b4aab6a341d611
snorm <snorm@MSI>
2024-09-14 14:47:26 -0500

    First Python commit

---
+++ main.py
@@ -0,0 +1,16 @@
+#a print statement in Python
+print("Hello")
+
+
+#a variable initialization
+name = "Joe Blow"
+print("Your name is: ", name)
+
+#reading in user input in Python
+favoriteColor = ""
+favoriteColor = input("What is your favorite color?")
+print("You said that your favorite color is: ", favoriteColor)
+
+#calculation example
+twentyFive = pow(5, 2)
+print(twentyFive)
